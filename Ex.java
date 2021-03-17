e8b88ef24a0f4e256ef47b642c56d6a0ecc764db
Sharanya <sha.sharu97@gmail.com>
2021-03-17 10:44:11 +0530

    Examp

---
+++ Ex.java
@@ -0,0 +1,18 @@
+class P{
+public static void main(String[] args) {
+		System.out.println("Inside P");
+		main(10);
+}
+
+
+public static void main(int a)
+ {
+		System.out.println("Inside P");
+  }     
+}
+
+class Q{
+public static void main(String[] args) {
+		System.out.println("Inside Q");
+}
+}
